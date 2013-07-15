c37b946dbed9bbf3f7452e1dd6bd08a083a1aae5
Ubuntu <ubuntu@ip-172-31-46-250.us-west-2.compute.internal>
2013-07-15 21:45:00 +0000

    read text from index.html trial 2

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var resText = fs.readFileSync('index.html')
 
 
 app.get('/', function(request, response) {  
-  response.send(resText.toStrin());
+  response.send(resText.toString());
 });
 
 var port = process.env.PORT || 5000;
